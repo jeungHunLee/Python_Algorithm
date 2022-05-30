a396bb7bbfaa97ce95b37ce6b3d503f0b9d15a78
jeungHunLee <ksjs1111@gmail.com>
2022-05-30 11:56:20 +0900

    그리디

--- 01.Greedy/11-1.py
+++ 01.Greedy/11-1.py
@@ -5,8 +5,8 @@ count = 0   # 그룹에 속한 인원수
 result = 0  # 그룹의 개수
 
 for i in data:
-    count += 1
-    if count >= i:
+    count += 1      # 그룹에 현재 모험가를 포함
+    if count >= i:      # 그룹의 인원수가 공포도보다 높거나 같을때 그룹 형성
         result += 1
         count = 0
 
