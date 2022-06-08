fc0c994ea0820ae46ebb4f3b3c4129bf9e8194e4
jeungHunLee <ksjs1111@gmail.com>
2022-06-08 11:29:20 +0900

    그리디

--- 01.Greedy/03-2.py
+++ 01.Greedy/03-2.py
@@ -1,22 +1,40 @@
 # 큰 수의 법칙
+# 내가 푼 풀이
 n, m, k = map(int, input().split())
-a = list(map(int, input().split()))
-a.sort(reverse=True)  # 내림 차순 정렬
+num = list(map(int, input().split()))
+num.sort()  # 오름차순 정렬
+count = 0  # 가장 큰 수를 더한 횟수를 저장할 변수
+result = 0
 
-first = a[0]
-second = a[1]
+for _ in range(m):
+    if count != k:
+        result += num[n - 1]
+        count += 1  # 가장 큰 수를 더할때 마다 횟수 +1
+    else:  # 가장 큰 수를 k만큼 더하였다면
+        result += num[n - 2]  # 두번째 큰 수 한번 더하기
+        count = 0  # count 초기화
+
+print(result)
+
+# 예시 풀이
+n, m, k = map(int, input().split())
+data = list(map(int, input().split()))
+
+data.sort()  # 오름차순 정렬
+first = data[n - 1]  # 가장 큰 수
+second = data[n - 2]  # 두 번째로 큰 수
 
 result = 0
 
 while True:
-    for _ in range(k):
+    for i in range(k):  # 가장 큰 수를 k번 만큼 더하기
         if m == 0:
             break
         result += first
-        m -= 1
+        m -= 1  # 더할때 마다 1씩 빼기
     if m == 0:
         break
-    result += second
+    result += second  # 두번째로 큰 수 한 번 더하기
     m -= 1
 
 print(result)
