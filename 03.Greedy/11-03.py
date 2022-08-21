ab740edb2f8ab68d2dac45694cbd52ae6ba36e5b
jeungHunLee <ksjs1111@gmail.com>
2022-08-21 22:27:48 +0900

    그리디

--- 03.Greedy/11-03.py
+++ 03.Greedy/11-03.py
@@ -4,13 +4,10 @@ s = input()
 count = 0
 
 for i in range(len(s) - 1):
-    if s[i] == s[i+1]:
-        continue
-    else:
-        count += 1  # 문자열이 다르면 구분
+    if s[i] != s[i + 1]:
+        count += 1
 
-result = (count + 1) // 2   # 처음과 끝이 다른 경우를 위하여 count에 1을 더하기
-print(result)
+print((count + 1) // 2)
 
 # 예시 답안
 data = input()
