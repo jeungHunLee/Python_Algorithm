425e367738a97941bdf953a08209a90861919b3e
jeungHunLee <ksjs1111@gmail.com>
2022-09-02 17:55:04 +0900

    그리디

--- 03.Greedy/11-05.py
+++ 03.Greedy/11-05.py
@@ -2,15 +2,14 @@
 # 나의 풀이
 n, m = map(int, input().split())
 data = list(map(int, input().split()))
-result = []
+count = 0
 
-for i in range(len(data) - 1):
-    for j in range(i+1, len(data)):
-        if data[i] == data[j]:  # 같은 무게일 경우 제외
-            continue
-        result.append([data[i], data[j]])   # 서로 다른 무게 일때, 경우의 수 추가
+for i in range(n - 1):
+    for j in range(i + 1, n):
+        if data[i] != data[j]:
+            count += 1
 
-print(len(result))
+print(count)
 
 # 예시 풀이
 n, m = map(int, input().split())
@@ -23,7 +22,7 @@ for x in data:
     array[x] += 1   # 각 무게에 해당하는 개수 카운트
 
 result = 0
-for i in range(1, m+1):
+for i in range(1, m + 1):
     n -= array[i]   # 무게가 i인 공을 선택하는 경우의 수 제거
     result += array[i] * n  # 무게가 i인 공을 선택하는 모든 경우의 수
 
