f84770851496b87344ddedc076d6184493aac7ff
jeungHunLee <ksjs1111@gmail.com>
2023-01-25 23:32:49 +0900

    구현

--- 04.Implementation/04-05.py
+++ 04.Implementation/04-05.py
@@ -1,4 +1,18 @@
 # 04-05 (럭키 스트레이트)
+# 나의 풀이
+score = input()
+head = score[:len(score) // 2]
+tail = score[len(score) // 2:]
+
+head = list(map(int, head))
+tail = list(map(int, tail))
+
+if sum(head) == sum(tail):
+    print("LUCKY")
+else:
+    print("READY")
+
+# 예시 풀이
 n = input()
 left = 0
 right = 0
